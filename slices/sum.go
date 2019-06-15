9b409cba745deca361c8193b278dd9348c3122a4
Mo <dabiggmoe@gmail.com>
2019-06-15 21:19:18 +0300

    added Sum(), SumAll() and SumAllTails() with tests

--- slices/sum.go
+++ slices/sum.go
@@ -9,3 +9,29 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+//SumAll returns a slice summing each slice in the input
+func SumAll(slices ...[]int) []int {
+	var sumAll []int
+
+	for _, slice := range slices {
+		sumAll = append(sumAll, Sum(slice))
+	}
+
+	return sumAll
+}
+
+func SumAllTails(slices ...[]int) []int {
+	var sums []int
+
+	for _, slice := range slices {
+		if len(slice) < 1 {
+			sums = append(sums, 0)
+			continue
+		}
+		sums = append(sums, Sum(slice[1:]))
+
+	}
+
+	return sums
+}
--- slices/sum_test.go
+++ slices/sum_test.go
@@ -1,21 +1,48 @@
 package slices
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestSum(t *testing.T) {
 
-	assertSum := func(t *testing.T, numbers []int, sum, expected int) {
-		if sum != expected {
-			t.Errorf("%v. got '%d' want '%d'", numbers, sum, expected)
+	assertSum := func(t *testing.T, numbers []int, sum, want int) {
+		if sum != want {
+			t.Errorf("%v. got '%d' want '%d'", numbers, sum, want)
 		}
 	}
 
 	t.Run("Sum of any numbers", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 		sum := Sum(numbers)
-		expected := 6
-		assertSum(t, numbers, sum, expected)
+		want := 6
+		assertSum(t, numbers, sum, want)
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	slice1 := []int{1, 2, 3}
+	slice2 := []int{5, 6, 7, 8}
+	slice3 := []int{}
+
+	want := []int{6, 26, 0}
+	got := SumAll(slice1, slice2, slice3)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v. %v %v %v", got, want, slice1, slice2, slice3)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	slice1 := []int{5}
+	slice2 := []int{1, 2, 3, 4}
+	slice3 := []int{}
+
+	want := []int{0, 9, 0}
+	got := SumAllTails(slice1, slice2, slice3)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v. %v %v %v", got, want, slice1, slice2, slice3)
+	}
 }
