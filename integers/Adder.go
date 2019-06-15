fb959a9425c82878a90e465356bfe1119720bf4f
Mo <dabiggmoe@gmail.com>
2019-06-15 16:53:28 +0300

    added integers/Adder.go and Adder_test.go

---
+++ integers/Adder.go
@@ -0,0 +1,8 @@
+//https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/integers
+package integers
+
+//Add adds two integers and return the sum
+func Add(x, y int) int {
+	sum := x + y
+	return sum
+}
