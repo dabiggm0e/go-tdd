a728a0b31bb7d60ff7dca38b4d0282a51038fc4e
Mo <dabiggmoe@gmail.com>
2019-06-16 09:18:14 +0300

    added Withdraw() and needed tests

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -1,6 +1,12 @@
 package pointers
 
+import "fmt"
+
 type Bitcoin float64
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%.2f BTC", b)
+}
 
 type Wallet struct {
 	balance Bitcoin
@@ -8,6 +14,10 @@ type Wallet struct {
 
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
 
 func (w *Wallet) Balance() Bitcoin {
--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -4,12 +4,35 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10.0))
-	got := wallet.Balance()
-	want := Bitcoin(10.0) //Cast the float amount to Bitcoin type
+	t.Run("Test Bitcoin deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10.0))
+		got := wallet.Balance()
+		want := Bitcoin(10.0) //Cast the float amount to Bitcoin type
 
-	if got != want {
-		t.Errorf("want %.2f got %.2f. %v", got, want, wallet)
-	}
+		if got != want {
+			t.Errorf("got %s want %s. %v", got, want, wallet)
+		}
+	})
+
+	t.Run("Test Withdraw Bitcoin", func(t *testing.T) {
+		wallet := Wallet{15.0}
+		wallet.Withdraw(Bitcoin(10.0))
+		got := wallet.Balance()
+		want := Bitcoin(5.0)
+
+		if got != want {
+			t.Errorf("got %s want %s. %v", got, want, wallet)
+		}
+	})
+
+	t.Run("Test printing balance", func(t *testing.T) {
+		wallet := Wallet{15.0}
+		got := wallet.Balance().String()
+		want := "15.00 BTC"
+
+		if got != want {
+			t.Errorf("got %s want %s. %v", got, want, wallet)
+		}
+	})
 }
