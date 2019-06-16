a73c3e121db7f91a72c9c4c43eda4f2ba99160f0
Mo <dabiggmoe@gmail.com>
2019-06-16 09:26:23 +0300

    refactored code by adding assertBalance()

--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -4,26 +4,27 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 
+	assertBalance := func(t *testing.T, wallet *Wallet, want Bitcoin) {
+		t.Helper()
+		if wallet.Balance() != want {
+			t.Errorf("got %s want %s. %v", wallet.Balance(), want, wallet)
+		}
+	}
+
 	t.Run("Test Bitcoin deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10.0))
-		got := wallet.Balance()
 		want := Bitcoin(10.0) //Cast the float amount to Bitcoin type
 
-		if got != want {
-			t.Errorf("got %s want %s. %v", got, want, wallet)
-		}
+		assertBalance(t, &wallet, want)
 	})
 
 	t.Run("Test Withdraw Bitcoin", func(t *testing.T) {
 		wallet := Wallet{15.0}
 		wallet.Withdraw(Bitcoin(10.0))
-		got := wallet.Balance()
 		want := Bitcoin(5.0)
 
-		if got != want {
-			t.Errorf("got %s want %s. %v", got, want, wallet)
-		}
+		assertBalance(t, &wallet, want)
 	})
 
 	t.Run("Test printing balance", func(t *testing.T) {
