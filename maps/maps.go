aa34ecd145b6b3c558b0406f384e9f2cd48bef06
Mo <dabiggmoe@gmail.com>
2019-06-17 00:48:22 +0300

    added Insert() and TestInsert() to maps package

--- maps/maps.go
+++ maps/maps.go
@@ -4,11 +4,26 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("Couldn't find the word")
+var (
+	ErrNotFound          = errors.New("Couldn't find the word")
+	ErrWordAlreadyExists = errors.New("Word already exists")
+)
 
 func (d Dictionary) Search(key string) (value string, err error) {
 	if _, ok := d[key]; !ok {
 		return "", ErrNotFound
 	}
 	return d[key], nil
+}
+
+func (d Dictionary) Insert(word, desc string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		d[word] = desc
+	case nil:
+		return ErrWordAlreadyExists
+	}
+
+	return nil
 }
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -23,8 +23,38 @@ func TestSearch(t *testing.T) {
 
 }
 
+func TestInsert(t *testing.T) {
+
+	t.Run("New word", func(t *testing.T) {
+		dict := Dictionary{}
+		word := "word"
+		want := "description"
+		err := dict.Insert(word, want)
+		assertErrors(t, err, nil)
+		assertDefinition(t, dict, word, want)
+	})
+
+	t.Run("Existing word", func(t *testing.T) {
+		word := "word"
+		want := "description"
+		dict := Dictionary{word: want}
+		err := dict.Insert(word, want)
+		assertErrors(t, err, ErrWordAlreadyExists)
+		assertDefinition(t, dict, word, want)
+	})
+
+}
+
 func assertErrors(t *testing.T, got, want error) {
 	t.Helper()
+
+	if got == want {
+		return
+	}
+
+	if got == ErrWordAlreadyExists {
+		t.Fatal(got.Error())
+	}
 
 	if got != want {
 		t.Errorf("got error '%s' want '%s'", got, want)
@@ -34,5 +64,16 @@ func assertStrings(t *testing.T, key, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got '%s' want '%s'. Key '%s'", got, want, key)
+	}
+}
+func assertDefinition(t *testing.T, dict Dictionary, word, want string) {
+	t.Helper()
+	got, err := dict.Search(word)
+
+	if err != nil {
+		t.Fatalf("Should find added word: %s", err)
+	}
+	if got != want {
+		t.Errorf("got '%s' want '%s'. %s", got, want, word)
 	}
 }
