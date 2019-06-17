af536a7fad2c2e49f67f5fe9e2eee9d75fb76c8f
Mo <dabiggmoe@gmail.com>
2019-06-17 16:21:39 +0300

    Refactord n fixed the CountDown() for loop poststatement from i++ to i--

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -6,11 +6,16 @@ import (
 	"os"
 )
 
+const (
+	countDownStart = 3
+	finalWord      = "Go"
+)
+
 func CountDown(writer io.Writer) {
-	for i := 3; i > 0; i++ {
-		fmt.Fprintf(writer, "%d\n", i)
+	for i := countDownStart; i > 0; i-- {
+		fmt.Fprintln(writer, i)
 	}
-	fmt.Fprint(writer, "Go")
+	fmt.Fprint(writer, finalWord)
 
 }
 
