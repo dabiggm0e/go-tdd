11195283dc7787250dcb5bf68f77d9155ffe5c35
Mo <dabiggmoe@gmail.com>
2019-06-18 20:02:32 +0300

    refactored subtest "Check a list of websites"

--- concurrency/concurreny_test.go
+++ concurrency/concurreny_test.go
@@ -1,6 +1,9 @@
 package concurrency
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func mockWebsiteChecker(Url string) bool {
 	if Url == "waat://furhurterwe.geds" {
@@ -11,29 +14,23 @@ func mockWebsiteChecker(Url string) bool {
 
 func TestCheckWebistes(t *testing.T) {
 
-	websitesList := []string{"http://www.google.com",
+	websites := []string{
+		"http://www.google.com",
 		"http://blog.gypsydave5.com",
 		"waat://furhurterwe.geds",
 	}
 
-	testset := []struct {
-		Name          string
-		Url           string
-		ShouldBeAlive bool
-	}{
-		{Name: "Google", Url: "http://www.google.com", ShouldBeAlive: true},
-		{Name: "blog", Url: "http://blog.gypsydave5.com", ShouldBeAlive: true},
-		{Name: "Invalid Url", Url: "waat://furhurterwe.geds", ShouldBeAlive: false},
+	want := map[string]bool{
+		"http://www.google.com":      true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
 	}
 
 	t.Run("Check a list of websites", func(t *testing.T) {
 
-		results := CheckWebsites(mockWebsiteChecker, websitesList)
-		for _, test := range testset {
-			got, ok := results[test.Url]
-			if got != test.ShouldBeAlive && ok {
-				t.Errorf("Is %v alive? Got %v, should be %v", test.Url, got, test.ShouldBeAlive)
-			}
+		got := CheckWebsites(mockWebsiteChecker, websites)
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
 		}
 	})
 }
