214be7064ebc3e66ca7073cd77cb5ab903c9c782
Mo <dabiggmoe@gmail.com>
2019-06-19 23:01:28 +0300

    added resultChannel to collect the websites check results

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,15 +1,27 @@
 package concurrency
 
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, websites []string) map[string]bool {
+
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range websites {
+		url := url
 		go func() { //// FIXME:
-			results[url] = wc(url)
-		}() //// FIXME:
+			//results[u] = wc(u)
+			resultChannel <- result{url, wc(url)}
+		}()
 	}
 
+	for i := 0; i < len(websites); i++ {
+		result := <-resultChannel
+		results[result.string] = result.bool
+	}
 	return results
 }
