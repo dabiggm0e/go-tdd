da946a3d611b3c33b4a30828d4b20af67938e342
Mo <dabiggmoe@gmail.com>
2019-06-24 21:54:51 +0300

    initial commit

---
+++ contexts/server_test.go
@@ -0,0 +1,29 @@
+package server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type SpyStore struct {
+	response string
+}
+
+func (s *SpyStore) Fetch() string {
+	return s.response
+}
+
+func TestHandler(t *testing.T) {
+	data := "Hello, world"
+	server := Server(&SpyStore{data})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	server.ServeHTTP(response, request)
+
+	if response.Body.String() != data {
+		t.Errorf("got '%s' want '%s'", response.Body.String(), data)
+	}
+}
