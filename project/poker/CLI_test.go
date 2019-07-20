dd56e5ec8487d975c3dc0fe0240bf86b01678a39
Mo <dabiggmoe@gmail.com>
2019-07-20 23:40:16 +0300

    DRYed and refactored

--- project/poker/CLI_test.go
+++ project/poker/CLI_test.go
@@ -10,14 +10,20 @@ func TestCLI(t *testing.T) {
 
 	cli.PlayPoker()
 
-	if len(playerstore.winCalls) < 1 {
+	want := "Ziggy"
+	assertPlayerWins(t, playerstore, want)
+
+}
+
+func assertPlayerWins(t *testing.T, store *StubPlayerStore, winner string) {
+	t.Helper()
+
+	if len(store.winCalls) != 1 {
 		t.Fatalf("expected a win call but didn't get any")
 	}
 
-	got := playerstore.winCalls[0]
-	want := "Ziggy"
-
-	if got != want {
-		t.Errorf("didn't record correct winner. Got %q want %q", got, want)
+	got := store.winCalls[0]
+	if got != winner {
+		t.Errorf("didn't record correct winner. Got %q want %q", got, winner)
 	}
 }
